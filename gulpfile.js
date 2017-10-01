e894d6bfa689c08da4538fd388240bcc2de6ea68
luckyjam <luckyjam01@gmail.com>
2017-10-02 00:02:06 +0300

    created separate css modules, added style for large-hero block

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,9 @@ var gulp = require('gulp');
 var watch = require('gulp-watch'); 
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
-var cssvars = require('postcss-simple-vars');
+var cssVars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
 
 gulp.task('default', function() {
   console.log('Hoorayj - you created a Gulp task.');
@@ -14,8 +15,8 @@ gulp.task('html', function() {
 });
 
 gulp.task('styles', function() {
-  return gulp.src('./app/assests/styles/styles.css')
-    .pipe(postcss([autoprefixer, cssvars, nested]))
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, autoprefixer, cssVars, nested]))
     .pipe(gulp.dest('./app/temp/styles'));
 });
 
