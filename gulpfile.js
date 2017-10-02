ff20eb9d0625ff7dbf56373ca4112d4bde47cfc9
luckyjam <luckyjam01@gmail.com>
2017-10-02 23:54:54 +0300

    Added styles for buttons. Added variables to css architecture. Added structure for gulp files. Started mobile first development by adding mixins to css.

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,4 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch'); 
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var cssVars = require('postcss-simple-vars');
-var nested = require('postcss-nested');
-var cssImport = require('postcss-import');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default', function() {
-  console.log('Hoorayj - you created a Gulp task.');
-});
 
-gulp.task('html', function() {
-  console.log('Something useful');
-});
-
-gulp.task('styles', function() {
-  return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, autoprefixer, cssVars, nested]))
-    .pipe(gulp.dest('./app/temp/styles'));
-});
-
-gulp.task('watch', function() {
- watch('./app/index.html', function() {
-   gulp.start('html');
- });
- watch('./app/assets/styles/**/*.css', function() {
-   gulp.start('styles');
- });
-});
